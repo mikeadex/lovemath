e929a5da2a916d164ac390d9b9865184ba334d15
mike <joko.adeleye@gmail.com>
2025-01-04 17:23:59 +0000

    addition now working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //  wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-const { run } = require("node:test");
+
 
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName('button');
@@ -12,18 +12,26 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert('You clicked Submit!');
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+    runGame('addition');
 })
 /****
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type $gameType`);
+        throw `Unknown game type $gameType, aborting!`;
+    }
 
 }
 function checkAnswer() {
@@ -42,7 +50,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
+
 
 }
 
